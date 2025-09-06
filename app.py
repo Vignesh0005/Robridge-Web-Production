2e994e11ec4ca1cc52a35e5dff6a998f79d6b343
Vignesh <vigneshv5@hotmail.com>
2025-09-06 22:57:25 +0530

    move Python files to root for Railway detection

---
+++ app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Main Flask application for Railway deployment
+"""
+
+import os
+import sys
+sys.path.append('backend')
+
+from backend.barcode_generator import app
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
